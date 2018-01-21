b6f395ddd49c7e1eb12ad2085d7d45437b0fe9a9
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-01-20 17:57:50 -0800

    init

---
+++ assets/js/search.js
@@ -0,0 +1,3 @@
+﻿var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"64":"Function"},"rows":[{"id":0,"kind":64,"name":"add","url":"globals.html#add","classes":"tsd-kind-function"}]};
