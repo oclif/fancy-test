744430bdc0b740ba00dea64f23fdf9197731f2e6
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-01-20 17:52:34 -0800

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+fancy-mocha
+===========
+
+extends mocha with helpful extensions
+
+[![Version](https://img.shields.io/npm/v/fancy-mocha.svg)](https://npmjs.org/package/fancy-mocha)
+[![CircleCI](https://circleci.com/gh/jdxcode/fancy-mocha/tree/master.svg?style=svg)](https://circleci.com/gh/jdxcode/fancy-mocha/tree/master)
+[![Appveyor CI](https://ci.appveyor.com/api/projects/status/github/jdxcode/fancy-mocha?branch=master&svg=true)](https://ci.appveyor.com/project/heroku/fancy-mocha/branch/master)
+[![Codecov](https://codecov.io/gh/jdxcode/fancy-mocha/branch/master/graph/badge.svg)](https://codecov.io/gh/jdxcode/fancy-mocha)
+[![Greenkeeper](https://badges.greenkeeper.io/jdxcode/fancy-mocha.svg)](https://greenkeeper.io/)
+[![Known Vulnerabilities](https://snyk.io/test/npm/fancy-mocha/badge.svg)](https://snyk.io/test/npm/fancy-mocha)
+[![Downloads/week](https://img.shields.io/npm/dw/fancy-mocha.svg)](https://npmjs.org/package/fancy-mocha)
+[![License](https://img.shields.io/npm/l/fancy-mocha.svg)](https://github.com/jdxcode/fancy-mocha/blob/master/package.json)
---
+++ package-scripts.js
@@ -0,0 +1,85 @@
+const {
+  concurrent,
+  crossEnv,
+  mkdirp,
+  series,
+  ifWindows,
+  ifNotWindows,
+} = require('nps-utils')
+const pjson = require('./package.json')
+const release = pjson.devDependencies.typedoc ? ['ci.release.semantic-release', 'ci.release.typedoc'] : ['ci.release.semantic-release']
+const script = (script, description) => description ? {script, description} : {script}
+const hidden = script => ({script, hiddenFromHelp: true})
+const unixOrWindows = (unix, windows) => series(ifNotWindows(unix), ifWindows(windows))
+
+let ciTests = [
+  'ci.test.eslint',
+  'ci.test.mocha',
+  'ci.test.tslint',
+]
+
+module.exports = {
+  scripts: {
+    build: 'rm -rf lib && tsc',
+    lint: {
+      default: concurrent.nps('lint.eslint', 'lint.commitlint', 'lint.tsc', 'lint.tslint'),
+      eslint: script('eslint .', 'lint js files'),
+      commitlint: script('commitlint --from origin/master', 'ensure that commits are in valid conventional-changelog format'),
+      tsc: script('tsc -p test --noEmit', 'syntax check with tsc'),
+      tslint: script('tslint -p test', 'lint ts files'),
+    },
+    test: {
+      default: script(concurrent.nps('lint', 'test.mocha'), 'lint and run all tests'),
+      series: script(series.nps('lint', 'test.mocha'), 'lint and run all tests in series'),
+      mocha: {
+        default: script('mocha --forbid-only "test/**/*.test.ts"', 'run all mocha tests'),
+        coverage: hidden(series.nps('test.mocha.nyc nps test.mocha')),
+        junit: hidden(series(
+          crossEnv('MOCHA_FILE="reports/mocha.xml" ') + series.nps('test.mocha.nyc nps \\"test.mocha --reporter mocha-junit-reporter\\"'),
+          series.nps('test.mocha.nyc report text-lcov > coverage.lcov'),
+        )),
+        nyc: hidden('nyc --nycrc-path node_modules/@dxcli/dev-nyc-config/.nycrc'),
+      },
+    },
+    ci: {
+      test: {
+        default: hidden(series(
+          mkdirp('reports'),
+          unixOrWindows(
+            concurrent.nps(...ciTests),
+            series.nps(...ciTests),
+          ),
+        )),
+        mocha: hidden(
+          unixOrWindows(
+            series.nps('test.mocha.junit'),
+            series.nps('test.mocha'),
+          )
+        ),
+        eslint: hidden(
+          unixOrWindows(
+            series.nps('lint.eslint --format junit --output-file reports/eslint.xml'),
+            series.nps('lint.eslint'),
+          )
+        ),
+        tslint: hidden(
+          unixOrWindows(
+            series.nps('lint.tslint --format junit > reports/tslint.xml'),
+            series.nps('lint.tslint'),
+          )
+        ),
+      },
+      release: {
+        default: hidden(series.nps(...release)),
+        'semantic-release': hidden('semantic-release -e @dxcli/dev-semantic-release'),
+        typedoc: hidden(series(
+          'git clone -b gh-pages $CIRCLE_REPOSITORY_URL gh-pages',
+          'typedoc --out /tmp/docs src/index.ts --excludeNotExported --mode file',
+          'rm -rf ./gh-pages/*',
+          'mv /tmp/docs/* ./gh-pages',
+          'cd gh-pages && git add . && git commit -m "updates from $CIRCLE_SHA1 [skip ci]" && git push',
+        )),
+      },
+    },
+  },
+}
