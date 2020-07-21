216d511cdd73b60094da2b481cbb2bc21b9aef6a
Stephan Kaag <stephan@ka.ag>
2020-07-21 10:32:28 +0200

    Satify linter

--- src/stub.ts
+++ src/stub.ts
@@ -3,14 +3,22 @@ import * as sinon from 'sinon'
 /**
  * mocks an object's property
  */
-export default function<T extends object, K extends keyof T> (object: T, path: K, value: any) {
-  if (object === undefined || path === undefined) throw new Error('should not be undefined')
+export default function <T extends object, K extends keyof T> (
+  object: T,
+  path: K,
+  value: any,
+) {
+  if (object === undefined || path === undefined)
+    throw new Error('should not be undefined')
 
   return {
-    run(ctx: {sandbox: sinon.SinonSandbox}) {
-      const sandbox = ctx.sandbox = ctx.sandbox || sinon.createSandbox()
-      sandbox.stub(object, path).value(value)
-    }, finally(ctx: {sandbox: sinon.SinonSandbox}) {
+    run(ctx: { sandbox: sinon.SinonSandbox }) {
+      if (!ctx.sandbox) {
+        ctx.sandbox = sinon.createSandbox()
+      }
+      ctx.sandbox.stub(object, path).value(value)
+    },
+    finally(ctx: { sandbox: sinon.SinonSandbox }) {
       ctx.sandbox.restore()
     },
   }
